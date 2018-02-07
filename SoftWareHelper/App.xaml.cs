7d42d2e51c89169976e1c5dfa25749af2aaf81a4
yanjinhuagood <yanjinhuamail@gmail.com>
2018-02-07 14:20:28 +0800

    first commit

---
+++ SoftWareHelper/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace SoftWareHelper
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+        }
+    }
+}
