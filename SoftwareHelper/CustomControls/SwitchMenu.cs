4770cc0b8710a2ceace0bedfa0e77123bd935ecf
yanjinhuamail@gmail.com <yanjinhuamail@gmail.com>
2021-08-14 22:22:21 +0800

    updater.

--- SoftwareHelper/CustomControls/SwitchMenu.cs
+++ SoftwareHelper/CustomControls/SwitchMenu.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
