9404494a05994c7b1171bb21ce72021895b3ecd3
闫驚鏵 <yanjinhuamail@163.com>
2026-02-27 01:04:04 +0800

    feat: WPFDevelopers 0.0.0.3-preview2 into 0.0.0.3-preview3

--- Nimble/Properties/AssemblyInfo.cs
+++ Nimble/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("驚鏵·闫")]
 [assembly: AssemblyProduct("Nimble")]
-[assembly: AssemblyCopyright("Copyright © WPFDevelopersOrg 2020")]
+[assembly: AssemblyCopyright("Copyright © WPFDevelopersOrg 2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // 可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.4")]
-[assembly: AssemblyFileVersion("1.0.0.4")]
+[assembly: AssemblyVersion("1.0.0.5")]
+[assembly: AssemblyFileVersion("1.0.0.5")]
