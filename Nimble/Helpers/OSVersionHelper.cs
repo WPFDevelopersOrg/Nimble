9a49045aab51193d4cb5ccb0c9b4513eea8b73f2
闫驚鏵（Jinhua Yan） <yanjinhuamail@gmail.com>
2026-03-01 22:09:57 +0800

    Create OSVersionHelper.cs

---
+++ Nimble/Helpers/OSVersionHelper.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Win32;
+
+namespace Nimble.Helpers
+{
+    public static class OSVersionHelper
+    {
+        public static bool IsWindows11()
+        {
+            try
+            {
+                string registryKey = @"SOFTWARE\Microsoft\Windows NT\CurrentVersion";
+                using (RegistryKey key = Registry.LocalMachine.OpenSubKey(registryKey))
+                {
+                    if (key != null)
+                    {
+                        object buildValue = key.GetValue("CurrentBuild");
+
+                        if (buildValue != null)
+                        {
+                            int buildNumber;
+                            if (int.TryParse(buildValue.ToString(), out buildNumber))
+                            {
+                                if (buildNumber >= 22000)
+                                {
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch
+            {
+            }
+
+            return false;
+        }
+    }
+}
